6ca420e154c5ede1963e2d91a7d706591206a3ca
Gabriel-Luminy <gabriel@ladet.Net>
2016-10-24 14:59:22 +0200

    Config Spring pour les tests OK

--- gouvlad/src/test/java/dao/impl/PersonDaoBdTest.java
+++ gouvlad/src/test/java/dao/impl/PersonDaoBdTest.java
@@ -1,30 +1,22 @@
 package dao.impl;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import dao.IPersonDao;
 
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/spring.xml")
 public class PersonDaoBdTest {
 	
 	@Autowired
 	IPersonDao dao;
-	
-	
-	@Before
-	public void init(){
-		 String conf = "spring.xml";
-	        AbstractApplicationContext context = new ClassPathXmlApplicationContext(conf);
-	        context.registerShutdownHook();
-
-	}
 	
 	@Test
 	public void test() {

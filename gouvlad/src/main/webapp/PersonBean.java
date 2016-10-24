5042e7adafcd5e4c79f1c2000a352f381ccadc12
Gabriel-Luminy <gabriel@ladet.Net>
2016-10-24 13:22:49 +0200

    init personBean

---
+++ gouvlad/src/main/webapp/PersonBean.java
@@ -0,0 +1,14 @@
+
+public class PersonBean {
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String siteweb;
+	private String dateNaissance;
+	private GroupBean groupe;
+	
+	
+
+}

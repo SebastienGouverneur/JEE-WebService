7765c1bad6992eb23a0171261198e0e66e813bde
SebastienGouverneur <sebastien.gouverneur13@gmail.com>
2016-10-24 13:29:17 +0200

    groupBean

---
+++ gouvlad/src/main/webapp/GroupBean.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+
+public class GroupBean {
+	
+	private int id;
+	private String monGroupe;
+	private List<PersonBean> listPerson;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMonGroupe() {
+		return monGroupe;
+	}
+	public void setMonGroupe(String monGroupe) {
+		this.monGroupe = monGroupe;
+	}
+	public List<PersonBean> getListPerson() {
+		return listPerson;
+	}
+	public void setListPerson(List<PersonBean> listPerson) {
+		this.listPerson = listPerson;
+	}
+
+}

bdda52c2907d624e67de2ce74044253cc5833a22
Gabriel-Luminy <gabriel@ladet.Net>
2016-10-24 13:28:14 +0200

    getters/setters personBean

--- gouvlad/src/main/webapp/PersonBean.java
+++ gouvlad/src/main/webapp/PersonBean.java
@@ -10,5 +10,62 @@ public class PersonBean {
 	private GroupBean groupe;
 	
 	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSiteweb() {
+		return siteweb;
+	}
+	
+	public void setSiteweb(String siteweb) {
+		this.siteweb = siteweb;
+	}
+	
+	public String getDateNaissance() {
+		return dateNaissance;
+	}
+	
+	public void setDateNaissance(String dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	
+	public GroupBean getGroupe() {
+		return groupe;
+	}
+	public void setGroupe(GroupBean groupe) {
+		this.groupe = groupe;
+	}
+	
+	
+	
 
 }
